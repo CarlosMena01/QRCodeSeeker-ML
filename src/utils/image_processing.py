ff919e85519990b9dba4791511895a0ac6d8f61e
CarlosMena01 <cmena@unal.edu.co>
2023-09-04 20:20:51 -0500

    [FEATURE] Alinged QR

--- src/utils/image_processing.py
+++ src/utils/image_processing.py
@@ -15,15 +15,58 @@ model_finger = YOLO(PATH_FINGER)
 def detect_qr_code(image, model = model_detection, threshold = 0.4):
     results = model(image)[0]
     for result in results.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = result
+        x1, y1, x2, y2, score, _ = result
         if score > threshold:
             new_image = image[int(y1):int(y2), int(x1):int(x2)]
-            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
             return new_image
         else:
             print(f"Little score: {score}")
     return None
-def extract_qr_code(image):
-    return image
+
+def extract_qr_code(image, model = model_finger, threshold = 0.5):
+    results = model(image)[0]
+
+    dots = []
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, _ = result
+        if score > threshold:
+            dot = (int((x1+x2)/2),int((y1+y2)/2))
+            dots.append(dot)
+
+    def get_rotacion_angle(coordenates, epsilon = 230*2 ):
+        p1, p2, p3 = coordenates
+        vector1 = np.array(p2) - np.array(p3)
+        vector2 = np.array(p1) - np.array(p3)
+        vector3 = np.array(p1) - np.array(p2)
+
+        if abs(np.dot(vector1,vector2)) <= epsilon:
+            if np.cross(np.append(vector1, 0), np.append(vector2, 0))[2]  < 0:
+                angulo_radianes = np.arctan2(*vector1)
+            else:
+                angulo_radianes = np.arctan2(*vector2)
+        elif abs(np.dot(vector3,vector2)) <= epsilon:
+            if np.cross(np.append(vector3, 0), np.append(vector2, 0))[2]  < 0:
+                angulo_radianes = np.arctan2(*vector3)
+            else:
+                angulo_radianes = np.arctan2(*vector2)
+        elif abs(np.dot(vector1,vector3)) <= epsilon:
+            if np.cross(np.append(vector1, 0), np.append(vector3, 0))[2]  < 0:
+                angulo_radianes = np.arctan2(*vector1)
+            else:
+                angulo_radianes = np.arctan2(*vector3)
+        else:
+            angulo_radianes = np.arctan2(*vector3)
+        angulo_grados = 180 - np.degrees(angulo_radianes)
+        return angulo_grados
+
+    rotation_angle = get_rotacion_angle(dots)
+
+    heigth, width = image.shape[:2]
+    center = (heigth // 2, width // 2)
+    matrix_rot = cv2.getRotationMatrix2D(center, rotation_angle, 1.0)
+    image_rotated = cv2.warpAffine(image, matrix_rot, (heigth, width), flags=cv2.INTER_LINEAR,  borderValue=(255,255,255))
+
+    return image_rotated
 def gen_qr_code(image):
     return image
